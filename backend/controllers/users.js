8f5d6e54a22d0611002cf8d0e81da67f7e7eff3f
MigelG <za4elovek@yandex.ru>
2022-03-08 17:57:46 +0300

    add dotenv

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+
+const { NODE_ENV, JWT_SECRET } = process.env;
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
@@ -113,7 +116,11 @@ module.exports.login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password, next)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key',
+        { expiresIn: '7d' },
+      );
 
       res.send({ token });
     })
